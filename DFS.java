c1b816f3d99a88592e7959137feedf0d616480f0
aat0022 <35584116+aat0022@users.noreply.github.com>
2018-12-08 20:47:30 -0600

    Add files via upload

---
+++ DFS.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class DFS {
+	LinkedList<Node> open = new LinkedList<>();
+	ArrayList<Node> closed = new ArrayList<>();
+
+	public ArrayList<Node> runAlgorithm(Node start, Node goal) {
+		open.push(start);
+		
+		//While open is not empty
+		while(open.size() != 0) {
+			
+			//Dequeue node and add it to closed
+			Node thisNode = open.pop();
+			closed.add(thisNode);
+			
+			//If this node is the goal node
+			if (thisNode.equals(goal)) {
+				//Exit: trace back through parents to create a path
+				boolean done = false;
+				Node currentNode = thisNode;
+				ArrayList<Node> path = new ArrayList<>();
+				
+				while (!done) {
+					if (currentNode.getParent() == null) {
+						path.add(currentNode);
+						done = true;
+					} else {
+						path.add(currentNode);
+						currentNode = currentNode.getParent();
+					}
+				}
+				Collections.reverse(path);
+				return path;
+			}
+			
+			ArrayList<NodeValue> neighbors = thisNode.getNeighborList();
+			
+			//Expand all successors
+			for(NodeValue neighbor : neighbors) {
+				Node neighborNode = neighbor.getNode();
+
+				//If the successor is not on open or closed, queue.
+				if (!open.contains(neighborNode) && !closed.contains(neighborNode)) {
+					neighborNode.setParent(thisNode);
+					open.push(neighborNode);
+				}
+			}
+		}
+		//exit with failure if not found by now
+		return new ArrayList<Node>();
+	}
+}
