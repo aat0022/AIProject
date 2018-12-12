965824f2918d9d392777537110605fb21353c2a8
aat0022 <35584116+aat0022@users.noreply.github.com>
2018-12-11 21:01:46 -0600

    Add files via upload

--- Experiment.java
+++ Experiment.java
@@ -1,14 +1,16 @@
 
 public class Experiment {
 	public static void main(String[] args) {
-		long time1 = System.currentTimeMillis();
 		Driver test = new Driver();
-		for (int x = 0; x < 100; x++) {
-			test.run();
-			System.out.println(x);
+		for (int y = 0; y < 10; y++) {
+			long time1 = System.currentTimeMillis();
+			for (int x = 0; x < 100; x++) {
+				test.run();
+				System.out.println(x);
+			}
+			long time2 = System.currentTimeMillis();
+			long experimentTime = time2 - time1;
+			System.out.print(experimentTime + ", ");
 		}
-		long time2 = System.currentTimeMillis();
-		long experimentTime = time2 - time1;
-		System.out.println("Time in milliseconds for 100 runs: " + experimentTime);
 	}
 }
